85383859221940244be229971aea84ab88f4aca0
Nina Angelo <ninacelie@gmail.com>
2020-02-15 14:50:56 -0600

    add welcome message and login methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions 
     set :session_secret, "yummy_grocery_list"
+    register Sinatra::Flash
   end
 
   get "/" do
-    erb :welcome
+    erb :login 
   end
+
+  def logged_in?
+    # true if user is logged in, otherwise false
+    !!current_user 
+  end 
+
+  def current_user
+    @cureent_user ||= User.find_by(id: session[user_id])
+  end 
+
+  def authorized_to_edit?(journal_entry)
+    journal_entry.user == current_user 
+  end 
+
+   # use this helper method to protect controller actions where user must be logged in to proceed
+  def redirect_if_not_logged_in
+    if !logged_in?
+      flash[:errors] = "You must be logged in to view the page you tried to view."
+      redirect '/'
+  end
+end
+
+  # use this helper method to avoid showing welcome, login, or signup page to a user that's already logged in
+  def redirect_if_logged_in
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+  end
+end
+
+end
+
+
+
+    
+  
 
 end
